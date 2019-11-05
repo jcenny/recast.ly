16643a9b53ccc445ceea1cca93afc6069976a329
jcenny <jcenny26@gmail.com>
2019-11-04 16:28:57 -0800

    Completed dynamic Video List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import exampleVideoData from './../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +15,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em><VideoList videos={exampleVideoData}/></em> view goes here</h5></div>
       </div>
     </div>
   </div>
